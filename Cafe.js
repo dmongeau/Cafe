db92928a997d0de36a9b6c4e9b0fbb232cc53ad3
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2011-04-16 23:12:55 -0400

    Basic file structure

--- Cafe.js
+++ Cafe.js
@@ -9,3 +9,43 @@
  *
  *
  */
+var Cafe = { 
+
+	/*
+	 *
+	 * Init function
+	 *
+	 */
+	'init' : function() {
+		
+	}
+};
+
+/*
+ *
+ * Check if jQyery is loaded
+ *
+ */
+Cafe.hasjQuery = function(){return (jQuery)?true:false;};
+
+/*
+ *
+ * Simple extend function
+ *
+ */
+Cafe.extend = function(obj1,obj2) {
+	if(Cafe.hasjQuery()) {
+		return jQuery.extend(obj1,obj2); 
+	}
+	
+	if (arguments.length > 2) {
+		for (var a = 1; a < arguments.length; a++) {
+			extend(obj1, arguments[a]);
+		}
+	} else {
+		for (var i in obj2) {
+			obj1[i] = obj2[i];
+		}
+	}
+	return obj1;
+};
