0842895fbef52a4785882bb20473394838334664
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2011-04-16 23:00:18 -0400

    Added main js file

---
+++ Cafe.js
@@ -0,0 +1,11 @@
+/*
+ *
+ * Café
+ *
+ * A javascript class that helps you organise your code using the MVC model
+ *
+ * @author David Mongeau-Petitpas <dmp@commun.ca>
+ * @version 0.1
+ *
+ *
+ */
